c54d81dac849f73022b6716b9674dab068c10ecc
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-04-24 11:23:08 -0700

    chore: Update README and ESLint config

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ESLint Rewrite
 
 Monorepo for the rewrite of ESLint.
+
+
+## Packages
+
+This repository is the home of the following packages:
+
+* [`@eslint/object-schema`](tree/main/packages/object-schema)
+* [`@eslint/config-array`](tree/main/packages/config-array)
--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,19 @@ import js from "@eslint/js";
 
 export default [
 	js.configs.recommended,
+	{
+		files: ["**/*.test.js"],
+		languageOptions: {
+			globals: {
+				describe: "readonly",
+				xdescribe: "readonly",
+				it: "readonly",
+				xit: "readonly",
+				beforeEach: "readonly",
+				afterEach: "readonly",
+				before: "readonly",
+				after: "readonly",
+			}
+		}
+	}
 ];
