27bb6e9b7a14b408bedddeec7bb603df7ae6676d
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-05-21 11:37:54 -0400

    feat: migrate-config package

---
+++ packages/migrate-config/tests/fixtures/release-it/expected.cjs
@@ -0,0 +1,62 @@
+const prettier = require("eslint-plugin-prettier");
+const _import = require("eslint-plugin-import");
+
+const { fixupPluginRules, fixupConfigRules } = require("@eslint/compat");
+
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+module.exports = [
+	...fixupConfigRules(
+		compat.extends(
+			"eslint:recommended",
+			"plugin:ava/recommended",
+			"prettier",
+		),
+	),
+	{
+		plugins: {
+			prettier,
+			import: fixupPluginRules(_import),
+		},
+
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+
+			ecmaVersion: 2020,
+			sourceType: "module",
+		},
+
+		rules: {
+			"prettier/prettier": 2,
+			"ava/no-ignored-test-files": 0,
+			"ava/no-import-test-files": 0,
+
+			"import/no-unresolved": [
+				2,
+				{
+					ignore: ["ava", "got"],
+				},
+			],
+
+			"import/no-unused-modules": 2,
+
+			"import/order": [
+				2,
+				{
+					"newlines-between": "never",
+				},
+			],
+		},
+	},
+];
