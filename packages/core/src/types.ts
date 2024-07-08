df3263b336b663b22be32bf0c499a70b378b5021
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-07-08 14:17:27 -0400

    feat: Add more type definitions (#81)
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -59,12 +59,56 @@ export interface Position {
 // Config
 //------------------------------------------------------------------------------
 
-export type RuleSeverity = 0 | 1 | 2 | "off" | "warn" | "error";
+/**
+ * The human readable severity level used in a configuration.
+ */
+export type SeverityName = "off" | "warn" | "error";
 
-export type RuleConfig = RuleSeverity | [RuleSeverity, ...any[]];
+/**
+ * The numeric severity level for a rule.
+ *
+ * - `0` means off.
+ * - `1` means warn.
+ * - `2` means error.
+ *
+ */
+export type SeverityLevel = 0 | 1 | 2;
 
+/**
+ * The severity of a rule in a configuration.
+ */
+export type Severity = SeverityName | SeverityLevel;
+
+/**
+ * Represents the configuration options for the core linter.
+ */
+export interface LinterOptionsConfig {
+	/**
+	 * Indicates whether or not inline configuration is evaluated.
+	 */
+	noInlineConfig?: boolean;
+
+	/**
+	 * Indicates what to do when an unused disable directive is found.
+	 */
+	reportUnusedDisableDirectives?: boolean | Severity;
+}
+
+/**
+ * Shared settings that are accessible from within plugins.
+ */
+export type SettingsConfig = Record<string, unknown>;
+
+/**
+ * The configuration for a rule.
+ */
+export type RuleConfig = Severity | [Severity, ...any[]];
+
+/**
+ * A collection of rules and their configurations.
+ */
 export interface RulesConfig {
-	[key: string]: RuleConfig;
+	[ruleId: string]: RuleConfig;
 }
 
 //------------------------------------------------------------------------------
