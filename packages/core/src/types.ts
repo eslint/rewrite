8df4bf4948ebef417e723d7e678573b6eaeb01a2
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-11-25 14:17:38 -0500

    fix: Update RuleVisitor type
    fixes #134

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -92,20 +92,19 @@ export type RuleType = "problem" | "suggestion" | "layout";
  */
 export type RuleFixType = "code" | "whitespace";
 
-/* eslint-disable @typescript-eslint/consistent-indexed-object-style -- Needs to be interface so people can extend it. */
 /* eslint-disable @typescript-eslint/no-explicit-any -- Necessary to allow subclasses to work correctly */
 /**
  * An object containing visitor information for a rule. Each method is either the
  * name of a node type or a selector, or is a method that will be called at specific
  * times during the traversal.
  */
-export interface RuleVisitor {
+export type RuleVisitor = {
 	/**
 	 * Called for each node in the AST or at specific times during the traversal.
 	 */
-	[key: string]: (...args: any[]) => void;
-}
-/* eslint-enable @typescript-eslint/consistent-indexed-object-style -- Needs to be interface so people can extend it. */
+	[key in string]?: (...args: any[]) => void;
+};
+
 /* eslint-enable @typescript-eslint/no-explicit-any -- Necessary to allow subclasses to work correctly */
 
 /**
