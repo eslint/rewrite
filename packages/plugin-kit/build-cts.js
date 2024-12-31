8d875caa06cad8291c5a2983fe64ea3b56c03485
Francesco Trotta <github@fasttime.org>
2024-12-31 14:38:39 +0100

    add file header comment

--- packages/plugin-kit/build-cts.js
+++ packages/plugin-kit/build-cts.js
@@ -1,3 +1,13 @@
+/**
+ * @fileoverview Build script for `index.d.cts`.
+ * This script creates "dist/cjs/index.d.cts" from "dist/esm/index.d.ts" by updating dynamic imports from `"./types.ts"`
+ * to include a `"resolution-mode": "import"` import attribute.
+ * It's not possible to add the import attributes directly to the source code because they are not allowed by jsr.
+ * jsr only includes the ESM types, where the import attributes are not required.
+ *
+ * @author Francesco Trotta
+ */
+
 import { readFile, writeFile } from "node:fs/promises";
 
 const oldSourceText = await readFile("dist/esm/index.d.ts", "utf-8");
