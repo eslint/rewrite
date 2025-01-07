38bc8e99a5995c3a6ffb4824accd9066311c172b
Francesco Trotta <github@fasttime.org>
2025-01-07 18:52:09 +0100

    use `types.cts`

--- packages/plugin-kit/build-cts.js
+++ packages/plugin-kit/build-cts.js
@@ -1,9 +1,7 @@
 /**
  * @fileoverview Rewrites import expressions for CommonJS compatibility.
- * This script creates "dist/cjs/index.d.cts" from "dist/esm/index.d.ts" by updating dynamic imports from `"./types.ts"`
- * to include a `"resolution-mode": "import"` import attribute.
- * It's not possible to add the import attributes directly to the source code because they are not allowed by jsr.
- * jsr only includes the ESM types, where the import attributes are not required.
+ * This script creates "dist/cjs/index.d.cts" from "dist/esm/index.d.ts" by modifying imports
+ * from `"./types.ts"` to `"./types.cts"`.
  *
  * @author Francesco Trotta
  */
@@ -13,6 +11,6 @@ import { readFile, writeFile } from "node:fs/promises";
 const oldSourceText = await readFile("dist/esm/index.d.ts", "utf-8");
 const newSourceText = oldSourceText.replaceAll(
 	'import("./types.ts")',
-	'import("./types.ts", { with: { "resolution-mode": "import" } })',
+	'import("./types.cts")',
 );
 await writeFile("dist/cjs/index.d.cts", newSourceText);
