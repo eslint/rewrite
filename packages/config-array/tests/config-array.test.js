b8bf8192c232b6ae98e2b62d332870b7715e18df
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-06-04 14:48:53 -0400

    ci: Test in Bun (#87)

--- packages/config-array/tests/config-array.test.js
+++ packages/config-array/tests/config-array.test.js
@@ -3524,9 +3524,11 @@ describe("ConfigArray", () => {
 
 		describe("push()", () => {
 			it("should throw an error when normalized", () => {
+				// Note: In Node.js the error message contains the word "extensible"
+				// In Bun the error message contains the word "readonly"
 				assert.throws(() => {
 					configs.push({});
-				}, /extensible/u);
+				}, /extensible|readonly/u);
 			});
 		});
 	});
