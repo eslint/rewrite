f17ea6e4f8949e29f4ec13f20f6e28586cab0a11
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-07-19 16:23:46 -0600

    remove comment

--- packages/config-helpers/tests/types/types.test.ts
+++ packages/config-helpers/tests/types/types.test.ts
@@ -20,75 +20,74 @@ defineConfig([], {});
 defineConfig([globalIgnores(["node_modules"])], []);
 defineConfig([{}]);
 defineConfig({
-	extends: [],
+  extends: [],
 });
 
 defineConfig({
-	rules: {
-		"no-console": "error",
-	},
+  rules: {
+    "no-console": "error",
+  },
 });
 
 defineConfig({
-	languageOptions: {
-		ecmaVersion: 2020,
-	},
+  languageOptions: {
+    ecmaVersion: 2020,
+  },
 });
 
 defineConfig({
-	extends: [
-		"js/recommended",
-		"react/recommended",
-		{ rules: { "no-console": "off" } },
-	],
+  extends: [
+    "js/recommended",
+    "react/recommended",
+    { rules: { "no-console": "off" } },
+  ],
 });
 
 defineConfig([
-	{
-		settings: {
-			react: {
-				version: "detect",
-			},
-		},
-	},
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ]);
 
 defineConfig(
-	{
-		extends: [
-			[
-				[{ rules: { "no-alert": "warn" } }],
-				{ rules: { "no-debugger": "error" } },
-			],
-			[
-				{ rules: { "no-eval": "error" } },
-				{ rules: { "no-implied-eval": "error" } },
-			],
-		],
-	},
-	globalIgnores(["node_modules"]),
+  {
+    extends: [
+      [
+        [{ rules: { "no-alert": "warn" } }],
+        { rules: { "no-debugger": "error" } },
+      ],
+      [
+        { rules: { "no-eval": "error" } },
+        { rules: { "no-implied-eval": "error" } },
+      ],
+    ],
+  },
+  globalIgnores(["node_modules"]),
 );
 
 globalIgnores(["node_modules"]);
 globalIgnores(["dist", "build"], "my name");
 
 defineConfig({
-	plugins: {
-	  'some-plugin': {
-		rules: {
-		  'some-rule': {
-			meta: {
-			  docs: {
-				recommended: 'not a boolean!' // <-- TS ERROR
-			  }
-			},
-  
-			create() {
-			  return {};
-			}
-		  }
-		}
-	  }
-	}  
-  }
-);
+  plugins: {
+    "some-plugin": {
+      rules: {
+        "some-rule": {
+          meta: {
+            docs: {
+              recommended: "not a boolean!",
+            },
+          },
+
+          create() {
+            return {};
+          },
+        },
+      },
+    },
+  },
+});
