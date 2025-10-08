6de203af58cf6cc574ceb416b3d30423350a8001
Francesco Trotta <github@fasttime.org>
2025-10-08 17:48:39 +0200

    improve `dedupe-types` tool

--- tools/dedupe-types.js
+++ tools/dedupe-types.js
@@ -25,23 +25,26 @@ const files = process.argv.slice(2);
 files.forEach(filePath => {
 	const lines = fs.readFileSync(filePath, "utf8").split(/\r?\n/gu);
 	const typedefs = new Set();
-	const importSpecs = new Set();
+	const importSources = new Set();
 	const outputLines = [];
 
 	for (const line of lines) {
 		const match = line.match(
-			/^(?<start>\/\*\*\s*@typedef\s+\{)import\("(?<importSpec>[^)]+)"\)(?<end>.*)/u,
+			/^(?<start>\/\*\*\s*@typedef\s+\{)import\("(?<importSource>.+?)"\)(?<end>.*)/u,
 		);
 		if (!match) {
+			// not a typedef, so just copy the line
 			outputLines.push(line);
 		} else if (!typedefs.has(line)) {
+			// we haven't seen this typedef before, so process it
 			typedefs.add(line);
-			const { start, importSpec, end } = match.groups;
-			const importName = `_${importSpec.replaceAll(/\W/gu, "")}`;
-			if (!importSpecs.has(importSpec)) {
-				importSpecs.add(importSpec);
+			const { start, importSource, end } = match.groups;
+			const importName = `_${importSource.replace(/\W/gu, "")}`;
+			if (!importSources.has(importSource)) {
+				// we haven't seen this import before, so add an @import comment
+				importSources.add(importSource);
 				outputLines.push(
-					`/** @import * as ${importName} from "${importSpec}"; */`,
+					`/** @import * as ${importName} from "${importSource}"; */`,
 				);
 			}
 			outputLines.push(`${start}${importName}${end}`);
