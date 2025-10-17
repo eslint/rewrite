f8df139631694431ecfc651e656932e283d4d14f
Francesco Trotta <github@fasttime.org>
2025-10-17 19:47:18 +0200

    fix: improve type support for isolated dependencies in pnpm (#289)

--- tools/dedupe-types.js
+++ tools/dedupe-types.js
@@ -25,19 +25,31 @@ const files = process.argv.slice(2);
 files.forEach(filePath => {
 	const lines = fs.readFileSync(filePath, "utf8").split(/\r?\n/gu);
 	const typedefs = new Set();
+	const importSources = new Set();
+	const outputLines = [];
 
-	const remainingLines = lines.filter(line => {
-		if (!line.startsWith("/** @typedef {import")) {
-			return true;
+	for (const line of lines) {
+		const match = line.match(
+			/^(?<start>\/\*\*\s*@typedef\s+\{)import\("(?<importSource>.+?)"\)(?<end>.*)/u,
+		);
+		if (!match) {
+			// not a typedef, so just copy the line
+			outputLines.push(line);
+		} else if (!typedefs.has(line)) {
+			// we haven't seen this typedef before, so process it
+			typedefs.add(line);
+			const { start, importSource, end } = match.groups;
+			const importName = `$${importSource.replace(/\W/gu, "")}`;
+			if (!importSources.has(importSource)) {
+				// we haven't seen this import before, so add an @import comment
+				importSources.add(importSource);
+				outputLines.push(
+					`/** @import * as ${importName} from "${importSource}"; */`,
+				);
+			}
+			outputLines.push(`${start}${importName}${end}`);
 		}
+	}
 
-		if (typedefs.has(line)) {
-			return false;
-		}
-
-		typedefs.add(line);
-		return true;
-	});
-
-	fs.writeFileSync(filePath, remainingLines.join("\n"), "utf8");
+	fs.writeFileSync(filePath, outputLines.join("\n"), "utf8");
 });
