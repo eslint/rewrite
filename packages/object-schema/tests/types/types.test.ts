9266041ba46fe7f19788c0aac9bd13f1691b6ea7
루밀LuMir <rpfos@naver.com>
2026-02-15 19:01:55 +0900

    fix: tighten types of `MergeStrategy` in `object-schema` (#348)

--- packages/object-schema/tests/types/types.test.ts
+++ packages/object-schema/tests/types/types.test.ts
@@ -10,6 +10,7 @@
 import {
 	type BuiltInMergeStrategy,
 	type BuiltInValidationStrategy,
+	MergeStrategy,
 	type ObjectDefinition,
 	ObjectSchema,
 	type PropertyDefinition,
@@ -18,6 +19,8 @@ import {
 //-----------------------------------------------------------------------------
 // Tests for BuiltInValidationStrategy
 //-----------------------------------------------------------------------------
+
+// #region BuiltInValidationStrategy
 
 const validationArray: BuiltInValidationStrategy = "array";
 const validationBoolean: BuiltInValidationStrategy = "boolean";
@@ -30,9 +33,13 @@ const validationNonEmptyString: BuiltInValidationStrategy = "string!";
 // @ts-expect-error -- Invalid validation strategy
 const invalidValidation: BuiltInValidationStrategy = "invalid";
 
+// #endregion BuiltInValidationStrategy
+
 //-----------------------------------------------------------------------------
 // Tests for BuiltInMergeStrategy
 //-----------------------------------------------------------------------------
+
+// #region BuiltInMergeStrategy
 
 const mergeAssign: BuiltInMergeStrategy = "assign";
 const mergeOverwrite: BuiltInMergeStrategy = "overwrite";
@@ -41,9 +48,93 @@ const mergeReplace: BuiltInMergeStrategy = "replace";
 // @ts-expect-error -- Invalid merge strategy
 const invalidMerge: BuiltInMergeStrategy = "invalid";
 
+// #endregion BuiltInMergeStrategy
+
+//-----------------------------------------------------------------------------
+// Tests for MergeStrategy
+//-----------------------------------------------------------------------------
+
+// #region MergeStrategy
+
+MergeStrategy.overwrite(1, 2) satisfies 2;
+MergeStrategy.overwrite("a", "b") satisfies "b";
+MergeStrategy.overwrite(true, false) satisfies false;
+MergeStrategy.overwrite({ a: 1 }, { b: 2 }) satisfies { b: 2 };
+
+// @ts-expect-error Type 'number' does not satisfy the expected type 'string'.
+MergeStrategy.overwrite(1, 2) satisfies string;
+// @ts-expect-error Type 'number' does not satisfy the expected type 'boolean'.
+MergeStrategy.overwrite(1, 2) satisfies boolean;
+// @ts-expect-error Type 'string' does not satisfy the expected type 'number'.
+MergeStrategy.overwrite("a", "b") satisfies number;
+// @ts-expect-error Type 'string' does not satisfy the expected type 'boolean'.
+MergeStrategy.overwrite("a", "b") satisfies boolean;
+// @ts-expect-error Type 'boolean' does not satisfy the expected type 'number'.
+MergeStrategy.overwrite(true, false) satisfies number;
+// @ts-expect-error Type 'boolean' does not satisfy the expected type 'string'.
+MergeStrategy.overwrite(true, false) satisfies string;
+
+MergeStrategy.replace(1, 2) satisfies number;
+MergeStrategy.replace("a", "b") satisfies string;
+MergeStrategy.replace(true, false) satisfies boolean;
+MergeStrategy.replace({ a: 1 }, { b: 2 }) satisfies Record<string, number>;
+
+// @ts-expect-error Type 'number' does not satisfy the expected type 'string'.
+MergeStrategy.replace(1, 2) satisfies string;
+// @ts-expect-error Type 'number' does not satisfy the expected type 'boolean'.
+MergeStrategy.replace(1, 2) satisfies boolean;
+// @ts-expect-error Type 'string' does not satisfy the expected type 'number'.
+MergeStrategy.replace("a", "b") satisfies number;
+// @ts-expect-error Type 'string' does not satisfy the expected type 'boolean'.
+MergeStrategy.replace("a", "b") satisfies boolean;
+// @ts-expect-error Type 'boolean' does not satisfy the expected type 'number'.
+MergeStrategy.replace(true, false) satisfies number;
+// @ts-expect-error Type 'boolean' does not satisfy the expected type 'string'.
+MergeStrategy.replace(true, false) satisfies string;
+
+const sym1: unique symbol = Symbol("sym1");
+const sym2: unique symbol = Symbol("sym2");
+MergeStrategy.assign({ [sym1]: 1 }, { [sym2]: true }) satisfies {
+	[sym1]: number;
+	[sym2]: boolean;
+};
+MergeStrategy.assign({ 1: 1 }, { 2: 2 }) satisfies {
+	1: number;
+	2: number;
+};
+MergeStrategy.assign({ a: 1 }, { b: 2 }) satisfies {
+	a: number;
+	b: number;
+};
+MergeStrategy.assign({ a: 1 } as const, { b: 2 } as const) satisfies {
+	readonly a: 1;
+	readonly b: 2;
+};
+MergeStrategy.assign({ a: 1 }, { a: "a" }) satisfies {
+	a: "a";
+};
+MergeStrategy.assign(undefined, { bar: true }) satisfies {
+	bar: true;
+};
+
+// @ts-expect-error Type 'number' is not assignable to parameter of type 'Record<string | number | symbol, unknown>'.
+MergeStrategy.assign(1, 2);
+// @ts-expect-error Type 'string' is not assignable to parameter of type 'Record<string | number | symbol, unknown>'.
+MergeStrategy.assign("a", "b");
+// @ts-expect-error Type 'boolean' is not assignable to parameter of type 'Record<string | number | symbol, unknown>'.
+MergeStrategy.assign(true, false);
+// @ts-expect-error `{ a: "a" }` should overwrite `{ a: 1 }`.
+MergeStrategy.assign({ a: 1 }, { a: "a" }) satisfies {
+	a: 1;
+};
+
+// #endregion MergeStrategy
+
 //-----------------------------------------------------------------------------
 // Tests for PropertyDefinition
 //-----------------------------------------------------------------------------
+
+// #region PropertyDefinition
 
 // PropertyDefinition with built-in strategies
 const propertyWithBuiltInStrategies: PropertyDefinition = {
@@ -99,9 +190,13 @@ const propertyWithSchema: PropertyDefinition = {
 
 propertyWithSchema.schema satisfies ObjectDefinition;
 
+// #endregion PropertyDefinition
+
 //-----------------------------------------------------------------------------
 // Tests for ObjectDefinition
 //-----------------------------------------------------------------------------
+
+// #region ObjectDefinition
 
 const emptyDefinition: ObjectDefinition = {};
 
@@ -132,9 +227,13 @@ const objectDefinition: ObjectDefinition = {
 
 objectDefinition satisfies Record<string, PropertyDefinition>;
 
+// #endregion ObjectDefinition
+
 //-----------------------------------------------------------------------------
 // Tests for ObjectSchema class
 //-----------------------------------------------------------------------------
+
+// #region ObjectSchema
 
 const schema = new ObjectSchema({
 	name: {
@@ -194,3 +293,5 @@ const schemaWithSubschema = new ObjectSchema({
 		},
 	},
 });
+
+// #endregion ObjectSchema
