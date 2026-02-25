5eb13534ce59df116f43aeede00a999b9ce5a554
Pixel998 <mrhbs998@gmail.com>
2026-02-26 01:55:57 +0300

    fix: correct extraConfigTypes validation error messages

--- packages/config-array/src/config-array.js
+++ packages/config-array/src/config-array.js
@@ -633,14 +633,14 @@ function assertNormalized(configArray) {
 function assertExtraConfigTypes(extraConfigTypes) {
 	if (extraConfigTypes.length > 2) {
 		throw new TypeError(
-			"configTypes must be an array with at most two items.",
+			"extraConfigTypes must be an array with at most two items.",
 		);
 	}
 
 	for (const configType of extraConfigTypes) {
 		if (!CONFIG_TYPES.has(configType)) {
 			throw new TypeError(
-				`Unexpected config type "${configType}" found. Expected one of: "object", "array", "function".`,
+				`Unexpected config type "${configType}" in extraConfigTypes. Expected one of: "array", "function".`,
 			);
 		}
 	}
